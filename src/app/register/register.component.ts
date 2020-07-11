44e7bb78ffa4caa89c601d868cf9a277d950f43f
Wolmir Nemitz <wolmir@caffeex.com>
2020-07-11 15:38:20 -0300

    Setup register route

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((values) => {
+      console.log(values);
+    });
   }
 
 }
