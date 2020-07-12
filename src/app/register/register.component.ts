a9d85e7635d33579c91c23cb8d67c00d59263f2f
Wolmir Nemitz <wolmir@caffeex.com>
2020-07-12 16:34:40 -0300

    Trigger build

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,14 +11,30 @@ export class RegisterComponent implements OnInit {
 
   public role: string;
 
+  firstNameControl: FormControl;
+  lastNameControl: FormControl;
+  addressControl: FormControl;
+  emailControl: FormControl;
+  passwordControl: FormControl;
+
   constructor(
     private route: ActivatedRoute
-  ) { }
+  ) {
+    this.firstNameControl = new FormControl('');
+    this.lastNameControl = new FormControl('');
+    this.addressControl = new FormControl('');
+    this.emailControl = new FormControl('');
+    this.passwordControl = new FormControl('');
+  }
 
   ngOnInit(): void {
     this.route.params.subscribe((values) => {
       this.role = values.role;
     });
+  }
+
+  register() {
+
   }
 
 }
